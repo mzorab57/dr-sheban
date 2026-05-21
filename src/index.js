0bb6d7dbe2d55afb4704e2bffdd5743d1096bce9
mzorab57 <mzorab57@gmail.com>
2026-05-21 16:53:31 +0300

    la brdni hash route kan

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import "./sass/style.scss";
 import App from "./App";
-import { HashRouter  } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+ <BrowserRouter>
     <I18nextProvider i18n={i18n}>
-      <HashRouter>
         <App />
-      </HashRouter>
     </I18nextProvider>
-  </React.StrictMode>
+ </BrowserRouter>
 );
